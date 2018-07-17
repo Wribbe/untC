29d3f30fea2b52b6b00997fbc62ff2eeb8f8fb79
Stefan Eng <eng_steff@hotmail.com>
2018-07-17 14:13:09 +0200

    Run test for 5 frames test, clean up test.c.

--- src/test.c
+++ src/test.c
@@ -2,9 +2,6 @@
 
 int tests_run = 0;
 
-int foo = 7;
-int bar = 4;
-
 bool
 test_init_lib()
 {
@@ -12,27 +9,36 @@ test_init_lib()
 }
 
 bool
-test_foo_bar()
+test_run_main_for_5()
 {
-  mu_assert(foo==bar, "foo %d, was not equal to bar %d!\n", foo, bar);
+  struct main_run_data data = {0};
+  size_t max_frames = 5;
+  data.max_frames = max_frames;
+  main_run(&data);
+  main_wait(&data);
+  mu_assert(data.total_frames == max_frames,
+      "Number of run frames, %zu, was not equal to requested %zu!\n",
+      max_frames, data.total_frames);
   return true;
 }
 
 bool
 all_tests() {
-  mu_run_test(test_foo_bar);
+  mu_run_test(test_init_lib);
+  mu_run_test(test_run_main_for_5);
   return true;
 }
 
 int
 main(void) {
   bool success = all_tests();
-  if (success) {
-    printf("NO ERRORS!\n");
-    return EXIT_SUCCESS;
-  } else {
-    printf("ERRORS!\n");
+  printf("Ran %d tests ", tests_run);
+  if (!success) {
+    printf("with Errors!:\n");
     ERR_PRINT();
     return EXIT_FAILURE;
+  } else {
+    printf("with no errors, all passed.\n");
+    return EXIT_SUCCESS;
   }
 }
