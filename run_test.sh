5bcd87503fe04ffdce3781d5db5b214ce8d834b5
Stefan Eng <eng_steff@hotmail.com>
2018-07-18 13:09:11 +0200

    Dump test output to log file, delete of no errors.

--- run_test.sh
+++ run_test.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 BIN_TEST='./bin/test'
-ERR_CODE_LEAK=11
+ERR_CODE=11
 ERR_CODE_SIGSEV=139
+FILE_LOG=test.log
 COMMAND_VALG="valgrind --suppressions=suppress.txt --leak-check=full
---error-exitcode=${ERR_CODE_LEAK}"
+--error-exitcode=${ERR_CODE}"
 # Build the latest files.
 make
 [ $? -eq 0 ] || { echo "COMPLIATION ERROR, ABORTING!" && exit; }
 # Run unit-tests with valgrind.
-${COMMAND_VALG} ${BIN_TEST}
+output=$(((((${COMMAND_VALG} ${BIN_TEST} 2>&1; echo $? >&3) | \
+  tee /dev/tty >&4) 3>&1) | (read xs; exit $xs)) 4>&1)
 retval=$?
+echo "$output" > $FILE_LOG
 # Check exit code.
 case $retval in
-  $ERR_CODE_LEAK)
-    echo "***** LEAK(S) DETECTED! *****"
+  $ERR_CODE)
+    echo "***** ERRORS DETECTED! *****"
     ;;
   $ERR_CODE_SIGSEV)
     echo "***** SEGMENTATION FAULT! *****"
     ;;
   *)
     echo "No errors."
+    rm $FILE_LOG
     ;;
 esac
