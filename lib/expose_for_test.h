8de0e211d83c1858dd7f8ffb41a467b39b5b4479
Stefan Eng <eng_steff@hotmail.com>
2018-08-06 13:42:26 +0200

    Add base64 string constants for testing purposes.

--- lib/expose_for_test.h
+++ lib/expose_for_test.h
@@ -4,3 +4,8 @@ click_rewind(struct v3 * pointer, size_t num_points);
 extern struct v3 * click_buffer_current;
 extern struct v3 * click_buffer_last;
 extern struct v3 * click_buffer_start;
+
+const char base64_test_src[] = "This is a text that should be used for testing\n"
+"encoding and decoding of base64 functionality.\n";
+
+const char base64_test_correct[] = "IlRoaXMgaXMgYSB0ZXh0IHRoYXQgc2hvdWxkIGJlIHVzZWQgZm9yIHRlc3RpbmdcbiIKImVuY29kaW5nIGFuZCBkZWNvZGluZyBvZiBiYXNlNjQgZnVuY3Rpb25hbGl0eS5cbiI=";
